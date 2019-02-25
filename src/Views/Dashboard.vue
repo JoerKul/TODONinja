4f6ac73350128522a94e550979e0f8bc7fd9190e
JoerKul <33197391+JoerKul@users.noreply.github.com>
2019-02-25 22:18:56 +0100

    add project table and team avatars

--- src/Views/Dashboard.vue
+++ src/Views/Dashboard.vue
@@ -2,12 +2,94 @@
     <div class="dashboard">
         <h1 class="subheading grey--text">Dashboard</h1>
 
-        <v-container class="my-3"> </v-container>
+        <v-container class="my-3">
+            <v-card v-for="project in projects" :key="project.title">
+                <v-layout row wrap :class="`pa-3 project ${project.status}`">
+                    <v-flex xs12 md6>
+                        <div class="caption grey--text">Project title</div>
+                        <div>{{ project.title }}</div>
+                    </v-flex>
+                    <v-flex xs6 sm4 md2>
+                        <div class="caption grey--text">Person</div>
+                        <div>{{ project.person }}</div>
+                    </v-flex>
+                    <v-flex xs6 sm4 md2>
+                        <div class="caption grey--text">Due by</div>
+                        <div>{{ project.due }}</div>
+                    </v-flex>
+                    <v-flex xs2 sm4 md2>
+                        <div class="right">
+                            <v-chip
+                                small
+                                :class="`${project.status} white--text caption`"
+                                >{{ project.status }}</v-chip
+                            >
+                        </div>
+                    </v-flex>
+                </v-layout>
+            </v-card>
+            <v-divider></v-divider>
+        </v-container>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+    data() {
+        return {
+            projects: [
+                {
+                    title: 'Tia Portal',
+                    person: 'Najibi Nagra',
+                    due: '12.03.2019',
+                    status: 'ongoing',
+                },
+                {
+                    title: 'Cordova',
+                    person: 'Tom Klarens',
+                    due: '11.01.2019',
+                    status: 'completed',
+                },
+                {
+                    title: 'Spirit',
+                    person: 'Robert Doyle',
+                    due: '01.02.2019',
+                    status: 'ongoing',
+                },
+                {
+                    title: 'Redux',
+                    person: 'Ista Nestec',
+                    due: '23.11.2018',
+                    status: 'overdue',
+                },
+            ],
+        }
+    },
+}
 </script>
 
-<style></style>
+<style>
+.project.completed {
+    border-left: 4px solid #3cd1c2;
+}
+
+.project.ongoing {
+    border-left: 4px solid orange;
+}
+
+.project.overdue {
+    border-left: 4px solid tomato;
+}
+
+.chip.completed {
+    background-color: #3cd1c2;
+}
+
+.chip.ongoing {
+    background-color: orange;
+}
+
+.chip.overdue {
+    background-color: tomato;
+}
+</style>
--- src/Views/Team.vue
+++ src/Views/Team.vue
@@ -2,19 +2,68 @@
     <div class="team">
         <h1 class="subheading grey--text">Team</h1>
 
-        <v-container class="my-3">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde
-                et harum esse corporis rerum ipsam, aliquam illo adipisci. Non
-                libero, doloribus repellendus quis vel modi omnis. Tempore
-                temporibus minima nostrum?
-            </p>
+        <v-container class="my-2">
+            <v-layout row wrap>
+                <v-flex
+                    xs12
+                    sm6
+                    md4
+                    lg3
+                    v-for="person in team"
+                    :key="person.name"
+                >
+                    <v-card class="text-xs-center ma-3">
+                        <v-responsive>
+                            <v-avatar size="100" class="ma-2 grey lighten-2">
+                                <img :src="person.avatar"/>
+                            </v-avatar>
+                        </v-responsive>
+                        <v-card-text>
+                            <div class="subheading">{{ person.name }}</div>
+                            <div class="grey--text">{{ person.role }}</div>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-btn flat color="grey">
+                                <v-icon small left>message</v-icon>
+                                <span>MESSAGE</span>
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+            </v-layout>
         </v-container>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+    data() {
+        return {
+            team: [
+                {
+                    name: 'Pauolo',
+                    role: 'Designer',
+                    avatar: 'https://randomuser.me/api/portraits/men/86.jpg',
+                },
+                {
+                    name: 'Marco',
+                    role: 'Web Developer',
+                    avatar: 'https://randomuser.me/api/portraits/men/35.jpg',
+                },
+                {
+                    name: 'Chui li',
+                    role: 'Social Media',
+                    avatar: 'https://randomuser.me/api/portraits/women/65.jpg',
+                },
+                {
+                    name: 'Stefano',
+                    role: 'Sales',
+                    avatar: 'https://randomuser.me/api/portraits/men/75.jpg',
+                },
+            ],
+        }
+    },
+}
 </script>
 
 <style></style>
